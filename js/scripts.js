54762570e77e1b1ed71026db67346f38cb443067
apple <apple@Celestes-IMac.local>
2019-11-20 20:42:51 -0800

    change bentley-boy.html to bentley-boy

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function() {
+    $("button#black").click(function() {
+        $("body").removeClass();
+        $("body").addClass("black-background");
+    });
+  
+    $("button#white").click(function() {
+        $("body").removeClass();
+        $("body").addClass("white-background");
+    });
+  });
